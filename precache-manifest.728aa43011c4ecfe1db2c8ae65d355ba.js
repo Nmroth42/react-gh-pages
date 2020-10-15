6275312aeea012a9bc5fd13ea9b7d02a7d02a378
Nmroth42 <Mannoroth42@gmail.com>
2020-10-15 13:23:09 +0700

    Updates

---
+++ precache-manifest.728aa43011c4ecfe1db2c8ae65d355ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ced8a8570e842e59fb5d",
+    "url": "/react-gh-pages/static/css/main.85036ed9.chunk.css"
+  },
+  {
+    "revision": "ced8a8570e842e59fb5d",
+    "url": "/react-gh-pages/static/js/main.ced8a857.chunk.js"
+  },
+  {
+    "revision": "70b8414e28aa35b5dbbe",
+    "url": "/react-gh-pages/static/js/1.70b8414e.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "c730a2691370b2ac0a8b28015c997022",
+    "url": "/react-gh-pages/index.html"
+  }
+];
