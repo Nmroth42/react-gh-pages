ca0e8c4d7cb0ae85a7d3ba7c741d9898bce540d8
Nmroth42 <Mannoroth42@gmail.com>
2020-10-18 12:16:19 +0700

    Updates

---
+++ precache-manifest.c0f6923702f9d7025428caed958d6d84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d067a19042290d82e346",
+    "url": "/react-gh-pages/static/css/main.cf139f1a.chunk.css"
+  },
+  {
+    "revision": "d067a19042290d82e346",
+    "url": "/react-gh-pages/static/js/main.d067a190.chunk.js"
+  },
+  {
+    "revision": "6bb8b91dff4f817ac7be",
+    "url": "/react-gh-pages/static/js/1.6bb8b91d.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "05be984f00b66c438a8fe4861273f796",
+    "url": "/react-gh-pages/index.html"
+  }
+];
